0957e4617a3eb3b29ea5848b198468449d1fadd7
ankitvillagercoder <112738840+aloneboyankit@users.noreply.github.com>
2024-02-24 07:40:55 +0530

    our server is setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,135 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbConnection.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("server error ", err);
+    });
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`server is running on  port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongo Db connection failed  !!!!!", error);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 /*
-
 (async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
